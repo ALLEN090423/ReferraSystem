cb1693045e1e8e3fc19268d9393869a299a4ebaa
asus <jw25aau@herts.ac.uk>
2025-12-29 22:23:20 +0000

    Force Swing UI language to English for all dialogs

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,32 @@
+import controller.ClinicianController;
 import controller.PatientController;
+import controller.ReferralController;
 import view.MainFrame;
 
 import javax.swing.*;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.ENGLISH);
+        UIManager.put("OptionPane.okButtonText", "OK");
+        UIManager.put("OptionPane.cancelButtonText", "Cancel");
+        UIManager.put("OptionPane.yesButtonText", "Yes");
+        UIManager.put("OptionPane.noButtonText", "No");
+
         SwingUtilities.invokeLater(() -> {
             try {
                 PatientController pc = new PatientController();
                 pc.load("data/patients.csv");
 
-                MainFrame frame = new MainFrame(pc);
+                ClinicianController cc = new ClinicianController();
+                cc.load("data/clinicians.csv");
+
+                ReferralController rc = new ReferralController();
+                rc.load("data/referrals.csv");
+
+                MainFrame frame = new MainFrame(pc, cc, rc);
                 frame.setVisible(true);
 
             } catch (Exception e) {
@@ -19,4 +35,3 @@ public class Main {
         });
     }
 }
-
--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -1,28 +1,29 @@
 package view;
 
+import controller.ClinicianController;
 import controller.PatientController;
+import controller.ReferralController;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class MainFrame extends JFrame {
 
-    public MainFrame(PatientController patientController) {
+    public MainFrame(PatientController patientController,
+                     ClinicianController clinicianController,
+                     ReferralController referralController) {
+
         setTitle("Healthcare Management System - Part 2");
-        setSize(1000, 650);
+        setSize(1100, 650);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
         JTabbedPane tabs = new JTabbedPane();
         tabs.addTab("Patients", new PatientPanel(patientController));
-
-
-        // tabs.addTab("Clinicians", new ClinicianPanel(...));
-        // tabs.addTab("Appointments", new AppointmentPanel(...));
-        // tabs.addTab("Prescriptions", new PrescriptionPanel(...));
-        // tabs.addTab("Referrals", new ReferralPanel(...));
+        tabs.addTab("Referrals", new ReferralPanel(referralController, patientController, clinicianController));
 
         setLayout(new BorderLayout());
         add(tabs, BorderLayout.CENTER);
     }
 }
+
